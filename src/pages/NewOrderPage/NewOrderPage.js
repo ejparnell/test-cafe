e2143cc9218172cc0079f8add9f637c8e0a2ed13
bethparnell <beth.parnell@generalassemb.ly>
2023-02-13 14:26:26 -0500

    End of react effect and ref hooks

--- src/pages/NewOrderPage/NewOrderPage.js
+++ src/pages/NewOrderPage/NewOrderPage.js
@@ -1,3 +1,55 @@
-export default function NewOrderPage() {
-	return <h1>NewOrderPage</h1>
+import { useState, useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'
+import './NewOrderPage.css'
+import * as itemsAPI from '../../utilities/items-api'
+import Logo from '../../components/Logo/Logo'
+import MenuList from '../../components/MenuList/MenuList'
+import CategoryList from '../../components/CategoryList/CategoryList'
+import OrderDetail from '../../components/OrderDetail/OrderDetail'
+import UserLogOut from '../../components/UserLogOut/UserLogOut'
+
+export default function NewOrderPage({ user, setUser }) {
+	const [menuItems, setMenuItems] = useState([])
+	const [activeCat, setActiveCat] = useState('')
+	const categoriesRef = useRef([])
+
+	useEffect(function () {
+		async function getItems() {
+			const items = await itemsAPI.getAll()
+
+			categoriesRef.current = items.reduce((cats, item) => {
+				const cat = item.category.name
+				return cats.includes(cat) ? cats : [...cats, cat] 
+			}, [])
+
+			setMenuItems(items)
+			setActiveCat(categoriesRef.current[0])
+		}
+		getItems()
+	}, [])
+
+	// useEffect(function() {
+	// 	console.log('useEffect runs when menuItems change')
+	// }, [menuItems])
+
+	return (
+		<div className='NewOrderPage'>
+			<aside>
+				<Logo />
+				<CategoryList
+					categories={categoriesRef.current}
+					activeCat={activeCat}
+					setActiveCat={setActiveCat}
+				/>
+				<Link to='/orders' className='button btn-sm'>
+					PREVIOUS ORDERS
+				</Link>
+				<UserLogOut user={user} setUser={setUser} />
+			</aside>
+			<MenuList
+				menuItems={menuItems.filter((item) => item.category.name === activeCat)}
+			/>
+			<OrderDetail />
+		</div>
+	)
 }
